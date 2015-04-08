28a6a0da41d0ed8ec5a9255e26954c11caf9873d
Schaffhauser Robin <r.schaffhauser@turing.u-strasbg.fr>
2015-04-08 14:05:06 +0200

    Creation de la page home

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var inscription = require('./routes/inscription');
+var home = require('./routes/home');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/inscription', inscription);
+app.use('/home', home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
