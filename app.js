b54dee19a63e540a2902433b6f549ba1abe210dd
Rizk Youssef <rizk@etu.unistra.fr>
2015-04-08 16:09:07 +0200

    Modification fichier JavaScript

--- app.js
+++ app.js
@@ -8,13 +8,10 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var inscription = require('./routes/inscription');
+var nouvinscr = require('./routes/nouvinscr');
 
-var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -26,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/inscription', inscription);
+app.use('/nouvinscr', inscription);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
