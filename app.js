8861694c74884ef8a267600ee416bf37bf8c326d
Rizk Youssef <rizk@etu.unistra.fr>
2015-04-22 14:32:10 +0200

    reglage de app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var nouvinscr = require('./routes/nouvinscr');
