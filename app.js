d2e4ab6ac60fd4c2f0bfff2cd3c1d517650c06d5
Rizk Youssef <rizk@etu.unistra.fr>
2015-04-22 15:43:43 +0200

    ajout de variable

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ var home = require('./routes/home');
 
 // view engine setup
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'jade');
 
 app.use('/', routes);
 app.use('/users', users);
