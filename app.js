eb15cfdf1f86e71fa64cb1889b59e0b557324554
Rizk Youssef <rizk@etu.unistra.fr>
2015-04-22 15:47:57 +0200

    ajout d'un app.set

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', routes);
 app.use('/users', users);
