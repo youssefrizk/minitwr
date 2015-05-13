b3e7d14bbaaffd4a1cdaccddea19402d922b2bfc
Rizk Youssef <rizk@etu.unistra.fr>
2015-05-13 15:33:54 +0200

    etablir le lien entre pagedeco2 et  index

--- routes/pagedeco2.js
+++ routes/pagedeco2.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pagedeco2', { title: 'Express' });
+  res.render('pagedeco2', { title: 'TweetRise' });
 });
 
 module.exports = router;
